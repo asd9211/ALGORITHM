b08dd67992c3dcef0a8f3181f87e0a3d8dedade3
asd9211 <drrg@naver.com>
2021-04-20 22:52:37 +0900

    programmers greedy3번 풀이

--- algorithm/src/algorithm/ProgGreedy3.java
+++ algorithm/src/algorithm/ProgGreedy3.java
@@ -2,35 +2,33 @@ package algorithm;
 
 
 public class ProgGreedy3 {
-
 	public String solution(String number, int k) {
 		// number-k 이상 index에서 제일 큰수
 		// number-k-1 이상 index에서 제일 큰수
-		String[] nm = number.split("");
-		boolean[] visited = new boolean[nm.length];
-		StringBuffer sb = new StringBuffer();
-		int len = nm.length;
+		//String[] nm = number.split("");
+		boolean[] visited = new boolean[number.length()];
+		StringBuilder sb = new StringBuilder();
+		int len = number.length();
 		int min = k;
 		int maxIdx = 0;
-		while(sb.length()!= (len-k)) {
-			int max = 0;
-			int siz = 0;
-			if(min > 0 && ((len-k) - sb.length()) <= min) min --;
-			siz = min;
-			for(int i = maxIdx ; i < (len-siz) ; i++) {
+		int size = len-k;
+		int max = 0;
+		while(sb.length()!= size) {
+			max = 0;
+			min++;
+			for(int i = maxIdx ; i < len-(len-min) ; i++) {
 				if(visited[i])continue;
-				if(max<Integer.parseInt(nm[i])) {
-					max = Integer.parseInt(nm[i]);
+				if(max < Character.getNumericValue(number.charAt(i))) {
+					max = Character.getNumericValue(number.charAt(i));
 					maxIdx = i;
 				}
 			}
-			visited[maxIdx] = true;
+			visited[maxIdx] = true; 
 			sb.append(max);
-			
-		}
-		String answer = sb.toString();
-		return answer;
+		} 
+		return sb.toString();
 	}
+	 
 	public static void main(String[] args) {
 		//"1924"	2 "94"
 		ProgGreedy3 t = new ProgGreedy3();
